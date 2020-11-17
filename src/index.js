99fdb3d81da10a9c7fe124f0c9a5903112a4c0ad
Michael Moreno <mmoreno2222@gmail.com>
2020-11-17 14:52:27 -0800

    Booklist renders manually with styling

--- src/index.js
+++ src/index.js
@@ -1,13 +1,41 @@
 import React from "react";
 import ReactDom from "react-dom";
+import './index.css'
 
 function Greeting() {
-  const Message = () => <h1>Hello World</h1>
+  const Image = () => {
+    return (
+      <img src="https://images-na.ssl-images-amazon.com/images/I/81h2gWPTYJL._AC_UL200_SR200,200_.jpg"></img>
+    );
+  };
+
+  const Title = () => {
+    return <h1>Becoming</h1>;
+  };
+
+  const Author = () => {
+    return <h2>Michelle Obama</h2>
+  }
+  
+  const Book = () => {
+    return (
+      <article className="book">
+        <Image />
+        <Title />
+        <Author />
+      </article>
+    );
+  };
 
   return (
-    <>
-      <Message />
-    </>
+    <section className='booklist'>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   );
 }
 

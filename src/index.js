bba1808edf996b77212195b9506a094af4d57eb7
Michael Moreno <mmoreno2222@gmail.com>
2020-11-17 15:05:23 -0800

    starting project with condensed code

--- src/index.js
+++ src/index.js
@@ -1,34 +1,22 @@
 import React from "react";
 import ReactDom from "react-dom";
-import './index.css';
+import "./index.css";
 
 function Greeting() {
-  const Image = () => {
-    return (
-      <img src="https://images-na.ssl-images-amazon.com/images/I/81h2gWPTYJL._AC_UL200_SR200,200_.jpg"></img>
-    );
-  };
-
-  const Title = () => {
-    return <h1>Becoming</h1>;
-  };
-
-  const Author = () => {
-    return <h2 style={{fontSize: "18px", marginTop: '0.25rem'}}>Michelle Obama</h2>
-  }
-  
   const Book = () => {
     return (
       <article className="book">
-        <Image />
-        <Title />
-        <Author />
+        <img src="https://images-na.ssl-images-amazon.com/images/I/81h2gWPTYJL._AC_UL200_SR200,200_.jpg"></img>
+        <h1>Becoming</h1>
+        <h2 style={{ fontSize: "18px", marginTop: "0.25rem" }}>
+          Michelle Obama
+        </h2>
       </article>
     );
   };
 
   return (
-    <section className='booklist'>
+    <section className="booklist">
       <Book />
       <Book />
       <Book />
